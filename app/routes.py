43ecdfb8f9d521b8c00721169ccbfe14d21912c8
JerryyZhu <jerrybzhu@gmail.com>
2020-07-26 17:34:28 +1000

    SQLAlchemy - Adding a Database

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,11 @@
 from flask import render_template
-from app import app
+from app import app, models
+
+Task = models.Task
 
 # http://www.website.com/
 @app.route('/')
 def index():
     title = 'My Personal Todo Application!'
-    tasks = [
-        {'name': 'Walk the dog'},
-        {'name': 'Buy some groceries'},
-        {'name': 'Listen to some music'},
-        {'name': 'Tidy up my room'}
-    ]
+    tasks = Task.query.all()
     return render_template("index.html", title=title, tasks=tasks)
