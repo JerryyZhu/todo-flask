cd38da963e9a79ff01cf3a42e382bbfb7e518c8c
JerryyZhu <jerrybzhu@gmail.com>
2020-07-26 21:45:30 +1000

    Adding Tasks

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,21 @@
-from flask import render_template
-from app import app, models
+from flask import render_template, request, redirect, url_for
+from app import app, models, db
 
 Task = models.Task
 
-# http://www.website.com/
 @app.route('/')
 def index():
     title = 'My Personal Todo Application!'
     tasks = Task.query.all()
     return render_template("index.html", title=title, tasks=tasks)
+
+# POST (Forms)
+@app.route('/item/', methods=['POST'])
+def add_item():
+    # Get form data from
+    taskName = request.form.get('itemName')
+    taskDescription = request.form.get('itemDescription')
+    new_item = Task(name=taskName, description=taskDescription)
+    db.session.add(new_item)
+    db.session.commit()
+    return redirect(url_for('index'))
